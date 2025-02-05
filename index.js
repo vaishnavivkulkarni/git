d662b4b577c15759b523980bf483db0f721a3b98
vaishnavivkulkarni <vaishnavivkulkarni20@gmail.com>
2025-02-05 08:55:36 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+import express from 'express';  // Use 'import' instead of 'require'
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Vaishnavi ');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
